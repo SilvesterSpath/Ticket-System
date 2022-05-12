c84b6d80e0381cf8ba161194101eb9027b95deea
SilvesterSpath <spath.sz@gmail.com>
2022-05-12 08:29:00 +0200

    setup .env

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();
+
+const PORT = process.env.NODE_ENV === 'dev' ? process.env.PORT : 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
+
 console.log('Server...');
