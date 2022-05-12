e5ca52cb70c5c2b329603f311aafd6646ff7238c
SilvesterSpath <spath.sz@gmail.com>
2022-05-12 08:34:07 +0200

    init express

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const dotenv = require('dotenv').config();
 
 const app = express();
 
+app.get('/api/users', (req, res) => {
+  res.status(200).send({ message: 'Hello' });
+});
+
 const PORT = process.env.NODE_ENV === 'dev' ? process.env.PORT : 3000;
 
 app.listen(PORT, () => {
