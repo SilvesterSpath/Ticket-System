193e61da25984224e1f5bd667f1b2eb9b4eeb99e
SilvesterSpath <spath.sz@gmail.com>
2022-05-16 06:25:10 +0200

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const routes = require('./routes/userRoutes');
 
 const app = express();
 
 app.get('/api/users', (req, res) => {
   res.status(200).send({ message: 'Hello' });
 });
+
+app.use('/api/users', routes);
 
 const PORT = process.env.NODE_ENV === 'dev' ? process.env.PORT : 3000;
 
