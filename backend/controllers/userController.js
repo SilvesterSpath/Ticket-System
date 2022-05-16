d180e893cc458985ff2b441a3fe27b54c923086a
SilvesterSpath <spath.sz@gmail.com>
2022-05-16 07:19:18 +0200

    error middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,19 @@
+// @desc   Register a new user
+// @route  /api/users
+// @access public
 const registerUser = (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error('Please include all fields');
+  }
+
   res.send('Register route');
 };
 
+// @desc   Login an existing user
+// @route  /api/users/login
+// @access public
 const loginUser = (req, res) => {
   res.send('Login route');
 };
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const routes = require('./routes/userRoutes');
+const errorHandler = require('./middleware/errorMiddleware');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/api/users', (req, res) => {
-  res.status(200).send({ message: 'Hello' });
+  res.status(200).send({ message: 'Welcome to the Ticket System API' });
 });
 
+// Routes
 app.use('/api/users', routes);
+
+app.use(errorHandler);
 
 const PORT = process.env.NODE_ENV === 'dev' ? process.env.PORT : 3000;
 
