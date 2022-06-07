2c37ff57ad2dd7498719bacda4152c222232ef7a
SilvesterSpath <spath.sz@gmail.com>
2022-06-07 07:29:09 +0200

    token init

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 
 // @desc   Register a new user
 // @route  /api/users
@@ -47,7 +48,21 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route  /api/users/login
 // @access public
 const loginUser = asyncHandler(async (req, res) => {
-  res.send('Login route');
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  // Check user and password match
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid credentials');
+  }
 });
 
 module.exports = {
