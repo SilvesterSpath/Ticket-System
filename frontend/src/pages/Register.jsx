0eee5c9e00bfe591dd38e2c76baccecf0d38dbfb
SilvesterSpath <spath.sz@gmail.com>
2022-06-09 08:12:53 +0200

    toast

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,10 +1,68 @@
-import React from 'react'
+import { useState } from 'react'
+import {FaUser} from 'react-icons/fa'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const {name, email, password, password2} = formData
+
+  const onChange = (e)=>{
+    setFormData((prev)=>({
+      ...prev,
+      [e.target.name]: e.target.value
+    }))
+    
+  }  
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    
+    if (password !== password2) {
+      toast.error('Passwords do not match');
+    }      
+    
+  }
+  
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <>
+     <section className="heading">
+      <h1>
+        <FaUser /> Register
+      </h1>
+      <p>Please create an account</p>
+     </section>
+
+     <section className="form">
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <input type="text" id='name' className="form-control" value={name} name='name' onChange={onChange} placeholder='Enter your name' required/>
+        </div>
+        <div className="form-group">
+          <input type="email" id='email' className="form-control" value={email} name='email' onChange={onChange} placeholder='Enter your email' required/>
+        </div>
+        <div className="form-group">
+          <input type="password" id='password' className="form-control" value={password} name='password' onChange={onChange} placeholder='Enter your password' required/>
+        </div>
+        <div className="form-group">
+          <input type="password" id='password2' className="form-control" value={password2} name='password2' onChange={onChange} placeholder='Confirm your password' required/>
+        </div>
+        <div className="form-group">
+          <button className="btn btn-block">Submit</button>
+          
+        </div>
+      </form>
+      
+      <ToastContainer />
+     </section>
+    </>
   )
 }
 
