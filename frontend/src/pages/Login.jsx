3c1f764f47ec53f01facb8a39dfe0aae05db863e
SilvesterSpath <spath.sz@gmail.com>
2022-06-13 06:05:24 +0200

    async thunk

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import {FaSignInAlt} from 'react-icons/fa'
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { login } from '../features/auth/authSlice';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -10,6 +12,10 @@ const Login = () => {
   })
 
   const { email, password} = formData
+
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isSuccess, message} = useSelector(state => state.auth)
 
   const onChange = (e)=>{
     setFormData((prev)=>({
@@ -20,8 +26,14 @@ const Login = () => {
   }  
 
   const onSubmit = (e) => {
-    e.preventDefault();  
-      
+    e.preventDefault();
+    
+    const userData = {      
+      email,
+      password        
+    }
+
+    dispatch(login(userData))
     
   }
   
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react'
 import {FaUser} from 'react-icons/fa'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { useSelector, useDispatch } from 'react-redux'
+import { register } from '../features/auth/authSlice'
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -12,6 +14,12 @@ const Register = () => {
   })
 
   const {name, email, password, password2} = formData
+
+  const dispatch = useDispatch()
+
+  const {user, isLoading, isSuccess, message} = useSelector((state) => 
+    (state.auth)
+  )
 
   const onChange = (e)=>{
     setFormData((prev)=>({
@@ -27,7 +35,15 @@ const Register = () => {
     
     if (password !== password2) {
       toast.error('Passwords do not match');
-    }      
+    } else {
+      const userData = {
+        name,
+        email,
+        password        
+      }
+
+      dispatch(register(userData))
+    }  
     
   }
   
@@ -35,7 +51,7 @@ const Register = () => {
     <>
      <section className="heading">
       <h1>
-        <FaUser /> Register
+        <FaUser /> Register 
       </h1>
       <p>Please create an account</p>
      </section>
