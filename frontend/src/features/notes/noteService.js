73f29799b428a5004f96e32e0eab19e28b7f7476
SilvesterSpath <spath.sz@gmail.com>
2022-07-29 09:52:13 +0200

    note reducer

---
+++ frontend/src/features/notes/noteService.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+const API_URL = '/api/tickets/:ticketId/notes';
+
+// Create new note
+const createNote = async (noteData, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.post(API_URL, noteData, config);
+
+  return response.data;
+};
+
+// Get notes from ticket
+const getNotes = async (token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL, config);
+
+  return response.data;
+};
+
+// Get note from ticket
+const getNote = async (ticketId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const response = await axios.get(API_URL + ticketId, config);
+
+  return response.data;
+};
+
+const noteService = {
+  createNote,
+  getNotes,
+  getNote,
+};
+
+export default noteService;
