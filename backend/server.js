2c574b94911d6c4d9e966cbb17042e23a1e346fd
SilvesterSpath <spath.sz@gmail.com>
2023-05-15 17:21:48 +0200

    static folder

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const color = require('colors');
 const dotenv = require('dotenv').config();
 const users = require('./routes/userRoutes');
+const path = require('path');
 
 const errorHandler = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
@@ -23,6 +24,16 @@ app.use('/api/users', users);
 app.use('/api/tickets', require('./routes/ticketRoutes'));
 
 app.use(errorHandler);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'))
+  );
+}
 
 const PORT = process.env.NODE_ENV === 'dev' ? process.env.PORT : 5000;
 
