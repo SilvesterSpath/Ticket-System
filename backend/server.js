2937ff514f8c44542768b03a09311c780ef636ac
SilvesterSpath <spath.sz@gmail.com>
2023-05-15 16:44:16 +0200

    port

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use('/api/tickets', require('./routes/ticketRoutes'));
 
 app.use(errorHandler);
 
-const PORT = process.env.NODE_ENV === 'dev' ? process.env.PORT : 3000;
+const PORT = process.env.NODE_ENV === 'dev' ? process.env.PORT : 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
