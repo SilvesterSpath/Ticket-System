6e7f85310de13eeaa613fb2efb3afcea56f31b93
SilvesterSpath <spath.sz@gmail.com>
2023-05-16 10:30:50 +0200

    remove console.log

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,90 +1,99 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import {useNavigate} from 'react-router-dom'
-import {toast} from 'react-toastify'
-import { createTicket, reset } from "../features/tickets/ticketSlice";
-import Spinner from '../components/Spinner'
-import BackButton from '../components/BackButton'
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { createTicket, reset } from '../features/tickets/ticketSlice';
+import Spinner from '../components/Spinner';
+import BackButton from '../components/BackButton';
 
 const NewTicket = () => {
-  const { user } = useSelector(store => store.auth)
-  const { isLoading, isError, isSuccess, message} = useSelector(store => store.tickets)
-  const [name] = useState(user.name)
-  const [email] = useState(user.email)
-  const [product, setProduct] = useState('iPhone')
-  const [description, setDescription] = useState('')
+  const { user } = useSelector((store) => store.auth);
+  const { isLoading, isError, isSuccess, message } = useSelector(
+    (store) => store.tickets
+  );
+  const [name] = useState(user.name);
+  const [email] = useState(user.email);
+  const [product, setProduct] = useState('iPhone');
+  const [description, setDescription] = useState('');
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  useEffect(()=>{
-    if(isError){
-      toast.error(message)
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
     }
 
-    if(isSuccess){
-      dispatch(reset())
-      navigate('/tickets')
+    if (isSuccess) {
+      dispatch(reset());
+      navigate('/tickets');
     }
 
-    dispatch(reset())
-  }, [isError, isSuccess, dispatch, navigate, message])
+    dispatch(reset());
+  }, [isError, isSuccess, dispatch, navigate, message]);
 
-  const onSubmit = (e)=>{
-    e.preventDefault()
-    dispatch(createTicket({product, description}))
-  }
-  
+  const onSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createTicket({ product, description }));
+  };
 
-  console.log(name, email, product, description);
-
-  if(isLoading){
-    return <Spinner />
+  if (isLoading) {
+    return <Spinner />;
   }
 
   return (
     <div>
-      <BackButton url='/'/>
-      <section className="heading">
+      <BackButton url='/' />
+      <section className='heading'>
         <h1>Create new Ticket</h1>
         <p>Please fill out the form below</p>
       </section>
 
-      <section className="form">
-        <div className="form-group">
-          <label htmlFor="name">Customer Name</label>
-          <input type="text" className="form-control" value={name} disabled />
+      <section className='form'>
+        <div className='form-group'>
+          <label htmlFor='name'>Customer Name</label>
+          <input type='text' className='form-control' value={name} disabled />
         </div>
-        <div className="form-group">
-          <label htmlFor="email">Customer Email</label>
-          <input type="text" className="form-control" value={email} disabled />
+        <div className='form-group'>
+          <label htmlFor='email'>Customer Email</label>
+          <input type='text' className='form-control' value={email} disabled />
         </div>
 
         <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="product">Product</label>
-          <select name="product" id="product" value={product} onChange={(e)=> setProduct(e.target.value)}>
-            <option value="iPhone">iPhone</option>
-            <option value="MacBook Pro">MacBook Pro</option>
-            <option value="iPad">iPad</option>
-            <option value="iMac">iMac</option>
-          </select>
+          <div className='form-group'>
+            <label htmlFor='product'>Product</label>
+            <select
+              name='product'
+              id='product'
+              value={product}
+              onChange={(e) => setProduct(e.target.value)}
+            >
+              <option value='iPhone'>iPhone</option>
+              <option value='MacBook Pro'>MacBook Pro</option>
+              <option value='iPad'>iPad</option>
+              <option value='iMac'>iMac</option>
+            </select>
           </div>
-          <div className="form-group">
-          <label htmlFor="description">Description of the issue</label>
-          <textarea  id="description" className="form-control" value={description} name='description' placeholder="Description" onChange={(e)=>{
-            setDescription(e.target.value)
-          }}/>
-            <div className="form-group">
-              <button className="btn btn-block">
-                Submit
-              </button>
+          <div className='form-group'>
+            <label htmlFor='description'>Description of the issue</label>
+            <textarea
+              id='description'
+              className='form-control'
+              value={description}
+              name='description'
+              placeholder='Description'
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            />
+            <div className='form-group'>
+              <button className='btn btn-block'>Submit</button>
             </div>
           </div>
         </form>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default NewTicket
+export default NewTicket;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,102 +1,136 @@
-import { useState, useEffect } from 'react'
-import {useNavigate} from 'react-router-dom'
-import {FaUser} from 'react-icons/fa'
-import { ToastContainer, toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
-import { useSelector, useDispatch } from 'react-redux'
-import { register, reset } from '../features/auth/authSlice'
-import Spinner from '../components/Spinner'
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { register, reset } from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 const Register = () => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: '',
-    password2: ''
-  })
+    password2: '',
+  });
 
-  const {name, email, password, password2} = formData
+  const { name, email, password, password2 } = formData;
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const {user, isLoading, isSuccess, isError, message} = useSelector((state) => 
-    (state.auth)
-  )
-  
-  useEffect(()=>{
-    if(isError){
-      toast.error(message)
+  const { user, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
     }
 
     // Redirect when logged in
-    if(isSuccess || user){
-      navigate('/')
+    if (isSuccess || user) {
+      navigate('/');
     }
 
-    dispatch(reset)
-  }, [isError, isSuccess, user, message, navigate, dispatch])
+    dispatch(reset);
+  }, [isError, isSuccess, user, message, navigate, dispatch]);
 
-  const onChange = (e)=>{
-    setFormData((prev)=>({
+  const onChange = (e) => {
+    setFormData((prev) => ({
       ...prev,
-      [e.target.name]: e.target.value
-    }))
-    
-  }  
+      [e.target.name]: e.target.value,
+    }));
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
 
-    
     if (password !== password2) {
       toast.error('Passwords do not match');
     } else {
       const userData = {
         name,
         email,
-        password        
-      }
+        password,
+      };
 
-      dispatch(register(userData))
-    }      
+      dispatch(register(userData));
+    }
+  };
+
+  if (isLoading) {
+    return <Spinner />;
   }
 
-  if(isLoading){
-    return <Spinner/>
-  }
-  
   return (
     <>
-     <section className="heading">
-      <h1>
-        <FaUser /> Register 
-      </h1>
-      <p>Please create an account</p>
-     </section>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
 
-     <section className="form">
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <input type="text" id='name' className="form-control" value={name} name='name' onChange={onChange} placeholder='Enter your name' required/>
-        </div>
-        <div className="form-group">
-          <input type="email" id='email' className="form-control" value={email} name='email' onChange={onChange} placeholder='Enter your email' required/>
-        </div>
-        <div className="form-group">
-          <input type="password" id='password' className="form-control" value={password} name='password' onChange={onChange} placeholder='Enter your password' required/>
-        </div>
-        <div className="form-group">
-          <input type="password" id='password2' className="form-control" value={password2} name='password2' onChange={onChange} placeholder='Confirm your password' required/>
-        </div>
-        <div className="form-group">
-          <button className="btn btn-block">Submit</button>          
-        </div>        
-      </form>
-      <ToastContainer />      
-     </section>
+      <section className='form'>
+        <form onSubmit={onSubmit}>
+          <div className='form-group'>
+            <input
+              type='text'
+              id='name'
+              className='form-control'
+              value={name}
+              name='name'
+              onChange={onChange}
+              placeholder='Enter your name'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              id='email'
+              className='form-control'
+              value={email}
+              name='email'
+              onChange={onChange}
+              placeholder='Enter your email'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              id='password'
+              className='form-control'
+              value={password}
+              name='password'
+              onChange={onChange}
+              placeholder='Enter your password'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              id='password2'
+              className='form-control'
+              value={password2}
+              name='password2'
+              onChange={onChange}
+              placeholder='Confirm your password'
+              required
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+        <ToastContainer />
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
